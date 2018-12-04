fa823505fa717b4c94a4c196cea3264278e21fd1
Marek N <marek.nekvasil@gmail.com>
2018-12-04 10:17:03 +0100

    initial commit

---
+++ 03.py
@@ -0,0 +1,8 @@
+import pandas
+
+data = pandas.read_csv('c:\\temp\\advent\\input_3_1.csv', header=None, sep='[#@,:x ]', usecols=[1, 4, 5, 7, 8]) #, index_col= 0 )
+print(data.head())
+
+fabric = [][]
+for i in data.items:
+    for x in range (i[ ])
