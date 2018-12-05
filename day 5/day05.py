b3cd2ffe59528a423cce2cab6beb56a35450d3a1
Marek N <marek.nekvasil@gmail.com>
2018-12-05 13:57:28 +0100

    code cleaned

--- day 5/day05.py
+++ day 5/day05.py
@@ -1,21 +1,46 @@
 def load_data():
+    """
+    Loads data from input file
+    :return:
+    """
     with open('05_input.txt', 'r') as f:
         return f.read().strip()
 
-def collapse(data: list) -> list:
+
+def collapse(polymer: str) -> str:
+    """
+    Collapses polymer string as per instruction on Day 5
+    :param polymer: Input polymer as a string
+    :return: Colapsed polymer string according to rules
+    """
+    test = list(polymer)
     i = 0
-    while i < len(data) - 1:
-        if abs(ord(data[i]) - ord(data[i + 1])) == 32:
-            data.pop(i + 1)
-            data.pop(i)
+    while i < len(test) - 1:
+        if abs(ord(test[i]) - ord(test[i + 1])) == 32:
+            test.pop(i + 1)
+            test.pop(i)
             i = max(i - 1, 0)
         else:
             i += 1
-    return data
-
-polymer = list(load_data())
-
-print('Answer to star 1 is: ', len(collapse(polymer)))
+    return ''.join(test)
 
 
+def leave_out(polymer: str) -> dict:
+    """
+    Tests leaving out individual letters in aplphabet on polymer string input, and collapsing it
+    :param polymer: Input polymer as a string
+    :return: Dictionary containing results pairs (Input Letter):(Length of reduced polymer)
+    """
+    res = dict()
+    for c in range(ord("a"), ord("z") + 1):
+        ch = chr(c)
+        res[ch] = len(collapse(polymer.replace(ch, '').replace(ch.upper(), '')))
+    return res
 
+
+if __name__ == "__main__":
+    data = load_data()
+    print('Answer to star 1 is: ', len(collapse(data)))
+
+    tst = leave_out(data)
+    print('Answer to star 2 is: ', min(tst.values()), ' on letter: ', min(tst, key=tst.get))
